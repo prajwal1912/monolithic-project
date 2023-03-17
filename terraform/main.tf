a71314b9071a5994fb1fbf2db626f96f7df5eb9a
prajwal isane <118800143+prajwal1912@users.noreply.github.com>
2023-03-17 16:43:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "key" {
 ami = "ami-0d81306eddc614a45"
 instance_type = "t2.micro"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
+key_name = "my-keypair"
 tags = {
 Name = "project-instace"
 Environment = "prod"
 }
-key_name = "my-keypair"
 }
 
